46a396a2bab1e0fa3d295cc9c2886eb953e7cecd
eltonew <elton.castro.new@gmail.com>
2022-06-21 13:57:36 -0300

    Desenvolvimento dos layouts de listagem e detalhe de partidas

---
+++ app/src/main/java/my/projetc/worldcupmatches/ui/DetailActivity.kt
@@ -0,0 +1,18 @@
+package my.projetc.worldcupmatches.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import my.projetc.worldcupmatches.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
